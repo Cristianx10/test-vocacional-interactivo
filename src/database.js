aeed6cae1d8bdc4daeb72cc28ef671398b674c77
Cristian Salguero <david_salinasx10@hotmail.com>
2019-10-06 12:17:52 -0500

    Conexion con base de datos

--- src/database.js
+++ src/database.js
@@ -1,32 +1,13 @@
+
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-crud-test';
+const URI = 'mongodb+srv://admin:temporalpassword@dbcluster-wtqbv.mongodb.net/admin?retryWrites=true&w=majority';
 
-const Auth =  {
-  auth:{
-    user:"cristianx10",
-    password:""
-  }
-};
-
-mongoose.connect(URI, Auth)
-  .then(db => console.log('Db is connected'))
+mongoose.connect(URI)
+  .then((cliente) =>{
+    console.log('Db is connected', cliente.db)
+  })
   .catch(error => console.error(error));
 
-
-
-  /*
-  // Connect using MongoClient
-MongoClient.connect(
-  "mongodb+srv://cluster0-lyy4u.mongodb.net/tienda",
-  
- 
-  , function(err, client) {
-  baseDatos = client.db(dbName);
-
-  app.listen(process.env.PORT || 1234);
-  //client.close();
-});
-
-*/
-
 module.exports = mongoose;
+
+
